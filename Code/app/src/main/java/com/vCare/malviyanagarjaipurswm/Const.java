cc04565bd6b4505d2406a278d49644f966185f7c
Vicky <mritunjaywevois@gmail.com>
2022-11-15 11:36:30 +0530

    Code Committed

---
+++ Code/app/src/main/java/com/vCare/malviyanagarjaipurswm/Const.java
@@ -0,0 +1,5 @@
+package com.vCare.malviyanagarjaipurswm;
+
+public class Const {
+    static String path = "https://jaipur-malviyanagar.firebaseio.com/";
+}
---
+++ Code/app/src/main/java/com/vCare/malviyanagarjaipurswm/MainScreen.java
@@ -0,0 +1,62 @@
+package com.vCare.malviyanagarjaipurswm;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.vCare.malviyanagarjaipurswm.Fragments.CardData;
+import com.vCare.malviyanagarjaipurswm.Fragments.ComplainPage;
+import com.vCare.malviyanagarjaipurswm.Fragments.HomePage;
+import com.vCare.malviyanagarjaipurswm.Fragments.LocationPage;
+import com.vCare.malviyanagarjaipurswm.Fragments.ProfilePage;
+
+public class MainScreen extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(this);
+        bottomNavigation.setSelectedItemId(R.id.homeBtn);
+
+    }
+    HomePage homePage = new HomePage();
+    ProfilePage profilePage = new ProfilePage();
+    ComplainPage complainPage = new ComplainPage();
+    LocationPage locationPage = new LocationPage();
+    CardData cardData = new CardData();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.homeBtn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.contains, homePage).commit();
+                return true;
+
+            case R.id.profileBtn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.contains, profilePage).commit();
+                return true;
+
+            case R.id.complaintsBtn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.contains, complainPage).commit();
+                return true;
+
+            case R.id.locationBtn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.contains, locationPage).commit();
+                return true;
+
+            case R.id.scanCardData:
+                getSupportFragmentManager().beginTransaction().replace(R.id.contains, cardData).commit();
+                return true;
+        }
+        return false;
+    }
+}
